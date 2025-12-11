3def5f3ad2c681e02481feb96e66df76e8c46ba9
77629296 <77629296@qq.com>
2025-12-11 23:11:52 +0800

    feat: add page

---
+++ app/[locale]/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+
+interface RootLayoutProps {
+  params: Promise<{ locale: string }>;
+  children: ReactElement;
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <div className="card w-full max-w-3xl min-h-[500px] max-h-[90vh] bg-white dark:bg-[#232324] shadow-xl border border-base-200 dark:border-slate-700 flex flex-col justify-between overflow-hidden max-sm:rounded-none max-sm:h-full rounded-lg">
+      <div className="flex-1 overflow-auto flex flex-col">{children}</div>
+    </div>
+  );
+}
