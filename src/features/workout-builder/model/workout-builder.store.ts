3568243595cb4f012e354f6357062564d33093b0
77629296 <77629296@qq.com>
2025-12-14 21:55:13 +0800

    feat: add workout stepper

---
+++ src/features/workout-builder/model/workout-builder.store.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+
+import { WorkoutBuilderStep } from '../types';
+
+interface WorkoutBuilderState {
+    currentStep: WorkoutBuilderStep;
+
+    // Actions
+    setStep: (step: WorkoutBuilderStep) => void;
+}
+
+export const useWorkoutBuilderStore = create<WorkoutBuilderState>((set, get) => ({
+    currentStep: 1 as WorkoutBuilderStep,
+
+    setStep: (step) => set({ currentStep: step }),
+}))
--- src/features/workout-builder/ui/workout-stepper.tsx
+++ src/features/workout-builder/ui/workout-stepper.tsx
@@ -1,6 +1,23 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { useWorkoutStepper } from '../hooks/use-workout-stepper'
 
 export function WorkoutStepper() {
-  return <Button >WorkoutStepper</Button>
+  const {
+    currentStep
+  } = useWorkoutStepper();
+  const renderTopBanner = () => {
+    if (currentStep === 1) {
+      return 1
+    }
+    if (currentStep === 2) {
+      return 2
+    }
+    if (currentStep === 3) {
+      return 3
+    }
+  }
+  return <div className="w-full max-w-6xl mx-auto h-full">
+    {renderTopBanner()}
+  </div>
 }
