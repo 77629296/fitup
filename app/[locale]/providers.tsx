aa476cdd25b083ed6ffc0fc51279a9bbaff0be5c
77629296 <77629296@qq.com>
2026-01-05 16:48:38 +0800

    feat: add queryClientProvider

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { I18nProviderClient } from "locales/client";
 import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+
+const queryClient = new QueryClient();
 
 export const Providers = ({ children, locale }: PropsWithChildren<{ locale: string }>) => {
   return (
     <>
       <NuqsAdapter>
-        <I18nProviderClient locale={locale}>
-          {children}
-        </I18nProviderClient>
+        <QueryClientProvider client={queryClient}>
+          <I18nProviderClient locale={locale}>
+            {children}
+          </I18nProviderClient>
+        </QueryClientProvider>
       </NuqsAdapter>
     </>
   )
